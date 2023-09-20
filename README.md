0dc53a0371a8b515e3bc562a84f9e0c8a50c4589
whoisprakhar44 <prakharshukla2505@gmail.com>
2023-09-20 18:28:54 +0530

    initialised project with necessary folders

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Diamond-Price-Prediction
+
+## End to End ML Project
+
+### Create an Environment
+
+```
+python3.10 -m venv env
+```
+
+### Activate Env
+
+```
+source env/bin/activate
+```
+
+### Install all Necessary Dependencies
+
+```
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e.'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readline()
+        requirements=[req.replace("\n", " ")for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    
+    return requirements
+
+setup (
+    name='RegressorProject',
+    version = '0.0.1',
+    author='Prakhar',
+    author_email='prakharshukla2505@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+)
